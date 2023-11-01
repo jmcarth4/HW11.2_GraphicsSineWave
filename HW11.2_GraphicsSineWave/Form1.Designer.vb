523b9f686d3cb2d0f28126a0f2ba049ce7106b02
jmcarth4@gmail.com <jmcarth4@gmail.com>
2023-11-01 17:26:23 -0600

    Add project files.

---
+++ HW11.2_GraphicsSineWave/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.QuitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.WaveformsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SineToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CosineToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TanToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.WhiteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.YellowToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BlueToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RedToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GreenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Black
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 33)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(800, 417)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.WaveformsToolStripMenuItem, Me.ColorsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 33)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.QuitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(54, 29)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'QuitToolStripMenuItem
+        '
+        Me.QuitToolStripMenuItem.Name = "QuitToolStripMenuItem"
+        Me.QuitToolStripMenuItem.Size = New System.Drawing.Size(148, 34)
+        Me.QuitToolStripMenuItem.Text = "&Quit"
+        '
+        'WaveformsToolStripMenuItem
+        '
+        Me.WaveformsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SineToolStripMenuItem, Me.CosineToolStripMenuItem, Me.TanToolStripMenuItem})
+        Me.WaveformsToolStripMenuItem.Name = "WaveformsToolStripMenuItem"
+        Me.WaveformsToolStripMenuItem.Size = New System.Drawing.Size(118, 29)
+        Me.WaveformsToolStripMenuItem.Text = "Waveforms"
+        '
+        'SineToolStripMenuItem
+        '
+        Me.SineToolStripMenuItem.Name = "SineToolStripMenuItem"
+        Me.SineToolStripMenuItem.Size = New System.Drawing.Size(167, 34)
+        Me.SineToolStripMenuItem.Text = "&Sine"
+        '
+        'CosineToolStripMenuItem
+        '
+        Me.CosineToolStripMenuItem.Name = "CosineToolStripMenuItem"
+        Me.CosineToolStripMenuItem.Size = New System.Drawing.Size(167, 34)
+        Me.CosineToolStripMenuItem.Text = "&Cosine"
+        '
+        'TanToolStripMenuItem
+        '
+        Me.TanToolStripMenuItem.Name = "TanToolStripMenuItem"
+        Me.TanToolStripMenuItem.Size = New System.Drawing.Size(167, 34)
+        Me.TanToolStripMenuItem.Text = "&Tan"
+        '
+        'ColorsToolStripMenuItem
+        '
+        Me.ColorsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.WhiteToolStripMenuItem, Me.YellowToolStripMenuItem, Me.BlueToolStripMenuItem, Me.RedToolStripMenuItem, Me.GreenToolStripMenuItem})
+        Me.ColorsToolStripMenuItem.Name = "ColorsToolStripMenuItem"
+        Me.ColorsToolStripMenuItem.Size = New System.Drawing.Size(79, 29)
+        Me.ColorsToolStripMenuItem.Text = "Colors"
+        '
+        'WhiteToolStripMenuItem
+        '
+        Me.WhiteToolStripMenuItem.Name = "WhiteToolStripMenuItem"
+        Me.WhiteToolStripMenuItem.Size = New System.Drawing.Size(163, 34)
+        Me.WhiteToolStripMenuItem.Text = "White"
+        '
+        'YellowToolStripMenuItem
+        '
+        Me.YellowToolStripMenuItem.Name = "YellowToolStripMenuItem"
+        Me.YellowToolStripMenuItem.Size = New System.Drawing.Size(163, 34)
+        Me.YellowToolStripMenuItem.Text = "Yellow"
+        '
+        'BlueToolStripMenuItem
+        '
+        Me.BlueToolStripMenuItem.Name = "BlueToolStripMenuItem"
+        Me.BlueToolStripMenuItem.Size = New System.Drawing.Size(163, 34)
+        Me.BlueToolStripMenuItem.Text = "Blue"
+        '
+        'RedToolStripMenuItem
+        '
+        Me.RedToolStripMenuItem.Name = "RedToolStripMenuItem"
+        Me.RedToolStripMenuItem.Size = New System.Drawing.Size(163, 34)
+        Me.RedToolStripMenuItem.Text = "Red"
+        '
+        'GreenToolStripMenuItem
+        '
+        Me.GreenToolStripMenuItem.Name = "GreenToolStripMenuItem"
+        Me.GreenToolStripMenuItem.Size = New System.Drawing.Size(163, 34)
+        Me.GreenToolStripMenuItem.Text = "Green"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents QuitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents WaveformsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SineToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CosineToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TanToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ColorsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents WhiteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents YellowToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BlueToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RedToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GreenToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ HW11.2_GraphicsSineWave/Form1.vb
@@ -0,0 +1,117 @@
+﻿'Jessica McArthur
+'RCET 3371
+'1 November 23
+'HW 11.2 Graph sine waves
+
+'Draws sine, tanget and cosine waves in picture box. Waveform resizes with form. 
+Public Class Form1
+    Public centerX, centerY, currentX, currentY, lastX, lastY As Integer      'Variables to plot line
+    Public penColor As Pen                                                    'Variable to set color of pen
+
+
+    Private Sub SineToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SineToolStripMenuItem.Click
+        Dim R, pi2, Vinst, stepX As Double                                    'Variables to calculate output
+        Dim Period, Vp, loopX As Integer
+
+        lastX = 0
+        lastY = 0
+        Me.Refresh()                                                          'Refresh picture box
+        centerX = PictureBox1.Size.Width / 2                                  'Calculation to produce Y variable
+        centerY = PictureBox1.Size.Height / 2
+        Period = centerX
+        lastX = 0
+        lastY = centerY
+        pi2 = (2 * Math.PI)
+        Vp = centerY / 2
+        stepX = centerX / 360
+
+        Do Until loopX > PictureBox1.Size.Width                               'Draw sine wave width of picture box
+            Vinst = centerY - (Vp * (Math.Sin(pi2 * (loopX / Period))))
+            PictureBox1.CreateGraphics.DrawLine(penColor, lastX, lastY, loopX, CInt(Vinst))
+            lastX = loopX
+            loopX += stepX
+            lastY = Vinst
+        Loop
+
+    End Sub
+
+    Private Sub TanToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TanToolStripMenuItem.Click
+        Dim R, pi2, Vinst, stepX As Double                                  'Variables to calculate output
+        Dim Period, Vp, loopX As Integer
+
+        lastX = 0
+        lastY = 0
+        Me.Refresh()                                                        'Refresh picture box
+        centerX = PictureBox1.Size.Width / 2                                'Calculation to produce Y variable
+        centerY = PictureBox1.Size.Height / 2
+        Period = centerX
+        lastX = 0
+        lastY = centerY
+        pi2 = (2 * Math.PI)
+        Vp = centerY / 20
+        stepX = centerX / 360
+        Do Until loopX > PictureBox1.Size.Width                             'Draw tangent wave width of picture box
+            Vinst = centerY - (Vp * (Math.Tan(pi2 * (loopX / Period))))
+            Try
+                PictureBox1.CreateGraphics.DrawLine(penColor, lastX, lastY, loopX, CInt(Vinst))
+                lastY = Vinst
+            Catch err As Exception
+            End Try
+            lastX = loopX
+            loopX += stepX
+        Loop
+    End Sub
+    Private Sub CosineToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CosineToolStripMenuItem.Click
+        Dim R, pi2, Vinst, stepX As Double                                  'Variables to calculate output
+        Dim Period, Vp, loopX As Integer
+
+        lastX = 0
+        lastY = 0
+        Me.Refresh()                                                        'Refresh picture box
+        centerX = PictureBox1.Size.Width / 2                                'Calculation to produce Y variable
+        centerY = PictureBox1.Size.Height / 2
+        Period = centerX
+        lastX = 0
+        lastY = centerY
+        pi2 = (2 * Math.PI)
+        Vp = centerY / 2
+        stepX = centerX / 360
+        Do Until loopX > PictureBox1.Size.Width                             'Draw cosine wave width of picture box
+            Vinst = centerY - (Vp * (Math.Cos(pi2 * (loopX / Period))))
+            If (Vinst - lastY) < -10 Then lastY = Vinst
+            PictureBox1.CreateGraphics.DrawLine(penColor, lastX, lastY, loopX, CInt(Vinst))
+            lastX = loopX
+            loopX += stepX
+            lastY = Vinst
+        Loop
+    End Sub
+
+    'Changes color of pen when color name selected in menu
+    Private Sub GreenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GreenToolStripMenuItem.Click
+        penColor = Pens.Green
+    End Sub
+
+    Private Sub BlueToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BlueToolStripMenuItem.Click
+        penColor = Pens.Blue
+    End Sub
+
+    Private Sub RedToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RedToolStripMenuItem.Click
+        penColor = Pens.Red
+    End Sub
+
+    Private Sub YellowToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles YellowToolStripMenuItem.Click
+        penColor = Pens.Yellow
+    End Sub
+
+    Private Sub WhiteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles WhiteToolStripMenuItem.Click
+        penColor = Pens.White
+    End Sub
+    'Ends program when 'quit' selected in menu
+    Private Sub QuitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles QuitToolStripMenuItem.Click
+        End
+    End Sub
+    'Loads default pen color to white
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        penColor = Pens.White
+    End Sub
+End Class
